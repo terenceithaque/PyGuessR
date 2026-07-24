e3df03b399dfdea00d8f6789717c0573bb7e0967
Terence Le Thierry d'Ennequin <terence.lethierry@protonmail.com>
2026-07-24 19:21:07 +0200

    Added level_1.json in the geographics folder

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 # Main application script
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QGridLayout
+
+
+class MainAppWindow(QMainWindow):
+    """Main application window."""
+
+    def __init__(self):
+        # Initialize the QMainWindow object
+        super.__init__()
+
+        # Layout for the widgets
+        display_layout = QGridLayout()
+
+        self.setLayout(display_layout)
+
+
+
+application = QApplication([])
+window = MainAppWindow()
+application.exec()
+
+
+        
