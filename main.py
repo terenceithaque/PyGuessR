b72888288f982c6ca3aa448094befd1bf9b73418
Terence Le Thierry d'Ennequin <terence.lethierry@protonmail.com>
2026-07-26 10:18:49 +0200

    Added a label to the main window in main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Main application script
-from PyQt6.QtWidgets import QApplication, QMainWindow, QGridLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QLabel
+from PyQt6.QtGui import QFont
+from PyQt6.QtCore import Qt
 from list_themes import *
 
 
@@ -13,19 +15,39 @@ class MainAppWindow(QMainWindow):
         # Set window title
         self.setWindowTitle("PyGuessR")
 
+        
+
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
         # Layout for the widgets
-        display_layout = QGridLayout()
+        parent_layout = QGridLayout(central_widget)
 
         available_themes = get_themes()
         print(available_themes)
 
         # Dictionnary associating themes with their absolute file paths
         theme_paths = {}
-
+        
         for theme in available_themes:
             theme_paths[theme] = get_theme_abspath(theme)
+        
+        print(theme_paths)
+        
 
-        print(theme_paths)    
+
+        theme_label = QLabel("Which theme do you want to explore today ?")
+        theme_label_font = QFont()
+        theme_label_font.setBold(True)
+        theme_label.setFont(theme_label_font)
+        theme_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+
+        
+
+        
+
+        parent_layout.addWidget(theme_label, 0, 0)
 
 
 
