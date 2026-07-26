8265cb3055274dc6ccb1970c74e474601ae929ae
Terence Le Thierry d'Ennequin <terence.lethierry@protonmail.com>
2026-07-26 10:58:04 +0200

    Modified comment in the __init__() method of the JSONQuizzFormat class

--- json_quizz.py
+++ json_quizz.py
@@ -3,11 +3,11 @@ import json
 
 
 class JSONQuizzFormat:
-    """This class acts as an abstraction for the JSON format which is used for the quizz."""
+    
 
     def __init__(self, json_file:str) -> None:
-        """Initializes the quizz in JSON format.
-        json_file : the JSON file from which the quizz content (questions, answers, etc.) is parsed by using the json module."""
+        """This class acts as an abstraction for the JSON format which is used for the quizz.\n
+        json_file: the JSON file from which the content of the quizz (questions, answers, etc.) is parsed using the json module."""
 
         self.file = json_file
 
